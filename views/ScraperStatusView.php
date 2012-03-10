5ddb215d41dcf494ed25bfe97ef05a12b444398d
Ben Russell <benrr101@csh.rit.edu>
2012-03-10 17:36:46 -0500

    date() doesn't work like I think it would...

--- views/ScraperStatusView.php
+++ views/ScraperStatusView.php
@@ -16,6 +16,41 @@ class ScraperStatusView extends View {
 
 	// METHODS /////////////////////////////////////////////////////////////
 
+	protected function timeElapsed($time) {
+		// Initialize the return string
+		$return = "";
+		
+		// Divide off days
+		$days = floor($time / (60 * 60 * 24));
+		if($days) {
+			$return .= "{$days} days ";
+			$time -= $days * 60 * 60 * 24;
+		}
+
+		// Divide off hours
+		$hours = floor($time / (60 * 60));
+		if($hours) {
+			$return .= "{$hours}:";
+			$time -= $hours * 60 * 60;
+		} else {
+			$return .= "00:";
+		}
+
+		// Divide off minutes
+		$mins = floor($time / 60);
+		if($mins) {
+			$return .= "{$mins}:";
+			$time -= $mins * 60;
+		} else {
+			$return .= "00:";
+		}
+
+		// Divide off seconds
+		$return .= str_pad($time, 2, "0", STR_PAD_LEFT);
+
+		return $return;
+	}
+
 	/**
 	 * Creates a new ScraperStatusView instance. It requires a StatusModel
 	 * @param	StatusModel	$model	StatusModel instance
