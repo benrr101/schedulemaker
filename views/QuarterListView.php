5578e616eae46711cfdb91241cbb066f969ab156
Ben Russell <benrr101@csh.rit.edu>
2012-03-17 16:22:59 -0400

    Adding a view for a quarter selection

---
+++ views/QuarterListView.php
@@ -0,0 +1,56 @@
+<?php
+////////////////////////////////////////////////////////////////////////////
+// SCHEDULEMAKER - Quarter Drop Down List
+//
+// @file	views/QuarterListView.php
+// @descrip	A simple to include dropdown list of quarters. Specify in the
+//			constructor what the name of the list is and an array of Quarter
+//			objects.
+// @author	Benjamin Russell (benrr101@csh.rit.edu)
+////////////////////////////////////////////////////////////////////////////
+
+class QuarterListView extends View {
+	// MEMBER VARIABLES ////////////////////////////////////////////////////
+	
+	// Quarter	The default quarter
+	private $default;
+
+	// string	The name of the select tag
+	private $name;
+
+	// array<Quarter>	The quarters to display in the view
+	private $quarters;
+
+	// METHODS /////////////////////////////////////////////////////////////
+	public function __construct($quarters, $default, $name = "quarter") {
+		// Store the information for view
+		$this->default = $default; // @TODO: Find error here
+		$this->name = $name;
+		$this->quarters = $quarters;
+	}
+
+	public function render() {
+		// Output the top of the select
+		echo "<select name='{$this->name}'>";
+	
+		// Iterate over the list of quarters and output each one
+		foreach($this->quarters as $quarter) {
+			// Verify that it's a quarter
+			if(!($quarter instanceof Quarter)) {
+				// @TODO: Error
+				throw new Exception("YA DUN GOOF'D");
+			}
+
+			$this->load("QuarterListRow", 
+				array(
+					'id'   => $quarter->getId(),
+					'name' => $quarter->getName(),
+					'selected' => ($quarter->getId() == $this->default->getId())
+					)
+				);
+		}
+
+		// Output the bottom of the select
+		echo "</select>";
+	}
+}
